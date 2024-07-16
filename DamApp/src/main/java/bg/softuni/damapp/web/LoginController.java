a2690e1491a9b7b3540c2d04fee3df642050932d
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-07-16 18:48:36 +0300

    Updating login.html and messages properties
    - Adding link to registration page for users without an account
    - Updating messages_bg.properties and messages_en.properties.

--- DamApp/src/main/java/bg/softuni/damapp/web/LoginController.java
+++ DamApp/src/main/java/bg/softuni/damapp/web/LoginController.java
@@ -1,5 +1,7 @@
 package bg.softuni.damapp.web;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private final MessageSource messageSource;
+
+    public LoginController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/login")
     public String login(@RequestParam(value = "error", required = false) String error,
                         Model model) {
 
         if (error != null) {
-            model.addAttribute("loginError", "Грешно име или парола!");
+            String errorMessage = messageSource.getMessage("login_error", null, LocaleContextHolder.getLocale());
+            model.addAttribute("loginError", errorMessage);
         }
 
         if (!model.containsAttribute("email")) {
